c9d634cb22127ca19babc1a79f8cffc41cf1290c
Russell Power <power@cs.nyu.edu>
2013-03-26 15:13:08 -0400

    Deprecate thyself.

--- src/mycloud/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-def Cluster(*args, **kw):
-  import mycloud.cluster
-  return mycloud.cluster.Cluster(*args, **kw)
--- src/mycloud/worker_main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import argparse
-from mycloud import worker
-
-if __name__ == '__main__':
-  worker.run_worker()
